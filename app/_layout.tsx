cf668a62ab19f3b248e414c8ebdcb605f608b297
Faniel Negasi <ugr18809916@office.mu.edu.et>
2026-09-03 23:09:02 -0400

    Refactor RootLayout to wrap Stack component with AuthProvider, enhancing context management and improving code structure

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,20 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+
 import { ThemeProvider } from "../context/ThemeContext";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <ThemeProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        />
+        <AuthProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </AuthProvider>
       </ThemeProvider>
     </SafeAreaProvider>
   );
